c8f6b45894109f21fb438a667bce02365641ee3d
lennarddaw <lennardgross07@gmail.com>
2025-05-11 14:05:58 +0200

    New

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,6 @@ import '../util/smart_device_box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
